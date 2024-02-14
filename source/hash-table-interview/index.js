61ed69c35b23e9098bb0066fb06cf65e793345f6
Touqeer Ahmed <Touqeerahmed1633@gmail.com>
2024-02-14 10:42:09 +0500

    hash

--- source/hash-table-interview/index.js
+++ source/hash-table-interview/index.js
@@ -1,19 +1,34 @@
-function itemInCommon(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-        console.log('yahan.... i', i)
-        for (let j = 0; j < arr2.length; j++) {
-            console.log('yahan.... j', j)
-            if (arr1[i] === arr2[j]) {
+// // for loop
+// function itemInCommon(arr1, arr2) {
+//     for(let i = 0; i < arr1.length; i++){
+//         for (let j = 0; j < arr2.length; j++) {
+//             if(arr1[i] === arr2[j]) return true
+//         }
+//     }
+//     return false;
+// }
 
-                return true
-            }
-        }
-    }
-    return false
+// let array1 = [1, 0, 5];
+// let array2 = [2, 4, 5];
+// itemInCommon(array1, array2);
+
+//More Efficient ways
+
+function itemInCommon(arr1, arr2) {
+  let obj = {};
+  for (let i = 0; i < arr1.length; i++) {
+    obj[arr1[i]] = true;
+  }
+  // { 1: true, 0: true, 9: true}
+
+  console.log({ obj });
+  for (let j = 0; j < arr2.length; j++) {
+    console.log("Is this ...", obj[arr2[j]]);
+    if (obj[arr2[j]]) return true;
+  }
+  return false;
 }
 
-
-let array1 = [1, 2, 5], array2 = [3, 4, 5]
-console.log(itemInCommon(array1, array2));
-
-
+let array1 = [1, 0, 5];
+let array2 = [3, 4, 2, 7, 9];
+itemInCommon(array1, array2);
