28eb4666e7f5ced246dca7b5fb3ff4cbcbe72fc2
Touqeer Admed <touqeerahmed1633@gmail.com>
2024-09-01 00:48:21 +0500

    wip basic 2

--- js-challenges/02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ js-challenges/02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,25 @@
-function arrayIntersection() {}
+// function arrayIntersection(arr1, arr2) {
+//   const intersectionArr = [];
 
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr2.includes(arr1[i]) && !intersectionArr.includes(arr1[i])) {
+//       intersectionArr.push(arr1[i]);
+//       console.log({ arr1: arr1[i] });
+//     }
+//   }
+//   return intersectionArr;
+// }
+
+function arrayIntersection(arr1, arr2) {
+  const set11 = new Set(arr1);
+  const intersectionArr = [];
+
+  for (let num of arr2) {
+    if (set11.has(num)) {
+      intersectionArr.push(num);
+    }
+  }
+
+  return intersectionArr;
+}
 module.exports = arrayIntersection;
--- js-challenges/02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ js-challenges/02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,19 @@
-function findMissingNumber() {}
+function findMissingNumber(numbers) {
+  if (numbers.length == 0) {
+    return 1;
+  }
+
+  const n = numbers.length + 1;
+  const expecedSum = (n * (n + 1)) / 2;
+
+  //   let actualSum = 0;
+  //   for (let index = 0; index < numbers.length; index++) {
+  //     actualSum += numbers[index];
+  //   }
+
+  const actualSum = numbers.reduce((sum, num) => sum + num, 0);
+
+  return expecedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
